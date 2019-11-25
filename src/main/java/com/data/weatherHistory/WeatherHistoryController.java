8b4b47d500d4787843472fdfd908b2b598fb1070
Bilal <bilal@mydomastudio.com>
2019-11-24 21:28:01 -0500

    updated error messages

--- src/main/java/com/data/weatherHistory/WeatherHistoryController.java
+++ src/main/java/com/data/weatherHistory/WeatherHistoryController.java
@@ -2,6 +2,8 @@ package com.data.weatherHistory;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WeatherHistoryController {
-
+  private static final Logger Log = LoggerFactory.getLogger(WeatherHistoryController.class);
+ 
   @Autowired
   private WeatherHistoryService WeatherHistoryService;
 
   @GetMapping("/weatherHistory")
   public String weatherHistory(@RequestParam(name="startTime", required=false) String startTime, @RequestParam(name="endTime", required=false) String endTime, Model model) {
+    Log.info("GET /weatherHistory");
     ArrayList<WeatherHistory> weatherHistoryList = new ArrayList<WeatherHistory>();
     if (startTime != null && startTime.length() > 0 && endTime != null && endTime.length() > 0) {
       weatherHistoryList = WeatherHistoryService.filter(startTime, endTime);
@@ -29,11 +33,14 @@ public class WeatherHistoryController {
 
   @GetMapping("/weatherHistory/{id}")
   public String weatherHistoryDetails(@PathVariable String id, Model model) {
+    Log.info("GET /weatherHistory/:id");
     try {
       Integer index = Integer.parseInt(id);
       model.addAttribute("weatherHistory", WeatherHistoryService.getWeatherHistory(index));
       return "weather-history-detail-page";
     } catch (NumberFormatException e) {
+      model.addAttribute("errorCode", "400 Bad Request");
+      model.addAttribute("errorMessage", "Invalid path variable id");
       return "error";
     }
   }
--- src/main/java/com/data/weatherHistory/WeatherHistoryService.java
+++ src/main/java/com/data/weatherHistory/WeatherHistoryService.java
@@ -4,11 +4,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component(value = "weatherHistoryService")
 public class WeatherHistoryService {
+  private static final Logger Log = LoggerFactory.getLogger(WeatherHistoryService.class);
   private static final String DATE_FORMAT = "mm/dd/yyyy";
 
   @Autowired
@@ -19,11 +22,13 @@ public class WeatherHistoryService {
   }
 
   public ArrayList<WeatherHistory> filter(String startTime, String endTime) {
+    Log.info("WeatherHistoryService.filter");
     try{
       Date minRange = new SimpleDateFormat(DATE_FORMAT).parse(startTime);
       Date maxRange = new SimpleDateFormat(DATE_FORMAT).parse(endTime);
       return weatherHistoryDao.filter(minRange, maxRange);
     } catch(Exception e) {
+      Log.warn(e.getMessage());
       return null;
     }
   }
